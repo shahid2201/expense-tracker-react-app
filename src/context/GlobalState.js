adb234f9220c29682df04cab4c856e4e96b6f062
Mohammad Shahid <shahimoh@sheridancollege.ca>
2024-12-01 05:49:41 -0500

    Add Add Transaction Feature

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer'
 //Initial State
 
 const initialState = {
-    transactions: [
-        {id: 1, text: 'Flower', amount: -20},
-        {id: 2, text: 'Flowerr', amount: -60},
-        {id: 3, text: 'Flowerrr', amount: 20},
-        {id: 4, text: 'Flowerrrr', amount: 40}
-    ]
+    transactions: []
 }
 
 //Create context
@@ -29,11 +24,19 @@ export const GlobalProvider = ({children}) => {
         })
     }
 
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+        })
+    }
+
     return(
         <GlobalContext.Provider value={{
             transactions: state.transactions, //when we use useState, we "initialize a variable state", which is an object that holds data about the components.
             //here we are creating a new variable transactions and accessing the current value of the already existing transactions from state object.
-            deleteTransaction
+            deleteTransaction,
+            addTransaction
         }}>
             {children}
         </GlobalContext.Provider>
